30124d2a59802aa4217bcc20374d737f976f7377
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2018-11-08 21:42:26 +0800

    start to build frame page

--- src/main/java/top/dustone/kaixin/controller/PageRouteController.java
+++ src/main/java/top/dustone/kaixin/controller/PageRouteController.java
@@ -9,4 +9,8 @@ public class PageRouteController {
     public String listDriver(){
         return "listDriver";
     }
+    @GetMapping(value = "/admin_home")
+    public String adminHome(){
+        return "/frame/admin/adminFrame";
+    }
 }
