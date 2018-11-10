79926b2c89852cf0ed945f89588c82414cb47124
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2018-11-10 20:31:00 +0800

    start to build frame page

--- front-page/resource/js/admin/adminFrame.js
+++ front-page/resource/js/admin/adminFrame.js
@@ -1,13 +1,16 @@
 //Global parameters
 var navData=[];
 var navDataTra=[];
+var navOpened=[];
 var navDataURL = "http://localhost:8080/kaixin/sys_makeDrawer";
 var leftDrawer;
+var tabBarIns;
 
 
 
 $(function() {
 	leftDrawer = new mdui.Drawer('#left-drawer');
+	tabBarIns = new mdui.Tab('#tabBar');
 	$("#drawButton").click(function() {
 		leftDrawer.toggle();
 	});
@@ -25,21 +28,48 @@ $(function() {
 			axios.get(navDataURL).then(function(response) {
 				self.drawerItems = response.data;
 			});
+		},
+		methods:{
+			chooseTab:function(event){
+				var tabId=event.target.getAttribute("tabId");
+				makeTab(tabId);
+			}
 		}
 	});
+	
 	mdui.mutation();
 });
-
+function makeTab(tabId){
+	if(navOpened.indexOf(tabId)==-1){
+		var navTab=findTabInfoById(tabId);
+		var tabBar='<a href="#tab'+tabId+'-window" id="tab'+tabId+'" class="mdui-ripple mdui-ripple-white">'+navTab.name+
+		'<button class="mdui-btn mdui-btn-block mdui-btn-icon mdui-btn-dense mdui-color-theme-accent mdui-ripple"><i class="mdui-icon material-icons">add</i></button></a>';
+		var tabWindow='<div id="tab'+tabId+'-window"><iframe url="www.baidu.com"></iframe></div>'
+		$("#tabBar").append(tabBar);
+		$("#tabWindows").append(tabWindow);
+		navOpened.push(tabId);
+		//rebuild the bar to refresh
+		tabBarIns = new mdui.Tab('#tabBar');
+		tabBarIns.show("tab"+tabId);
+	}else{
+		tabBarIns.show("tab"+tabId);
+	}
+}
+function findTabInfoById(tabId){
+	for(var i=0,len=navDataTra.length;i<len;i++){
+		if(navDataTra[i].id==tabId){
+			return navDataTra[i];
+		}
+	}
+}
 function transformNavData(){
-	alert(navData.length);
 	for(var i=0,len=navData.length;i<len;i++){
 		
 		var each=navData[i].sysDrawers;
-		navDataTra.push(each);
-// 		for(var j=0;len1=each.length;j<len1;j++){
-// 			
-// 		}
+		for(var j=0,len1=each.length;j<len1;j++){
+			navDataTra.push(each[j]);
+		}
 	}
-	//console.log(navDataTra);
+	console.log(navDataTra);
 }
 
