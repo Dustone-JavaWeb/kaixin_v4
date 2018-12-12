fbed68c58c2475a7a8f3209f42f5c6425a8d6914
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2018-12-12 16:47:44 +0800

    第五次重构 放弃iframe 使用单页面设计

--- src/main/java/top/dustone/kaixin/controller/PageRouteController.java
+++ src/main/java/top/dustone/kaixin/controller/PageRouteController.java
@@ -5,21 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageRouteController {
-    @GetMapping(value = "/admin_home")
-    public String adminHome(){
-        return "/page/admin/adminFrame";
-    }
     @GetMapping(value = "/admin_driver_list")
     public String listDriver(){
-        return "/page/admin/driverList";
+        return "listDriver";
     }
-    @GetMapping(value = "/admin_machine_list")
-    public String listMachine(){
-        return "/page/admin/machineList";
+    @GetMapping(value = "/admin_home")
+    public String adminHome(){
+        return "/frame/admin/adminFrame";
     }
-    @GetMapping(value = "/admin_machine_type_list")
-    public String listMachineType(){
-        return "/page/admin/machineTypeList";
-    }
-
 }
