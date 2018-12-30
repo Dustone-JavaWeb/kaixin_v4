752fd220ecfdd0566b840d530390cfdb2c2f480a
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2018-12-30 16:32:43 +0800

    资源文件管理模块开发

--- font-page-suit/webapp/js/admin/frame/fileManage.js
+++ font-page-suit/webapp/js/admin/frame/fileManage.js
@@ -39,6 +39,9 @@ $(function() {
 						position: "right-bottom",
 					});
 					if (response.data.t != null) {
+						if (self.entity.resource.resources == null) {
+							self.entity.resource.resources = [];
+						}
 						self.entity.resource.resources.push(response.data.t);
 					}
 					mdui.mutation();
@@ -99,6 +102,18 @@ $(function() {
 				} else {
 					return "";
 				}
+			},
+			renderSize: function(value) {
+				if (null == value || value == '') {
+					return "0 Bytes";
+				}
+				var unitArr = new Array("Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+				var index = 0;
+				var srcsize = parseFloat(value);
+				index = Math.floor(Math.log(srcsize) / Math.log(1024));
+				var size = srcsize / Math.pow(1024, index);
+				size = size.toFixed(2); //保留的小数位数
+				return size + unitArr[index];
 			}
 		},
 	});
