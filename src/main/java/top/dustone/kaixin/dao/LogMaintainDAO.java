ebb8e44e55a8e112b06c075baf0cadf0f8c3845f
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2019-01-05 08:21:14 +0800

    开始完善日志模块开发

--- src/main/java/top/dustone/kaixin/dao/LogMaintainDAO.java
+++ src/main/java/top/dustone/kaixin/dao/LogMaintainDAO.java
@@ -1,7 +1,8 @@
 package top.dustone.kaixin.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import top.dustone.kaixin.entity.log.LogMaintain;
 
-public interface LogMaintainDAO extends JpaRepository<LogMaintain,Integer> {
+public interface LogMaintainDAO extends JpaRepository<LogMaintain,Integer>,JpaSpecificationExecutor<LogMaintain> {
 }
