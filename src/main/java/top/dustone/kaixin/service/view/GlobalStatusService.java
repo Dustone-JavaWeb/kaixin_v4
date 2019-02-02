383d0bb74451b155370911aab471dae112bb42d2
Dustone-JavaWeb <35624201+Dustone-JavaWeb@users.noreply.github.com>
2019-02-02 21:40:51 +0800

    控制台基本完成

--- src/main/java/top/dustone/kaixin/service/view/GlobalStatusService.java
+++ src/main/java/top/dustone/kaixin/service/view/GlobalStatusService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.dustone.kaixin.dao.view.DriverStatusCountDAO;
 import top.dustone.kaixin.dao.view.MachineStatusCountDAO;
+import top.dustone.kaixin.dao.view.WorkCountDAO;
 import top.dustone.kaixin.util.BaseboardData;
 
 @Service
@@ -12,10 +13,13 @@ public class GlobalStatusService {
     MachineStatusCountDAO machineStatusCountDAO;
     @Autowired
     DriverStatusCountDAO driverStatusCountDAO;
+    @Autowired
+    WorkCountDAO workCountDAO;
     public BaseboardData makeBaseboardData(){
         BaseboardData baseboardData=new BaseboardData();
         baseboardData.setDriverStatusCounts(driverStatusCountDAO.findAll());
         baseboardData.setMachineStatusCounts(machineStatusCountDAO.findAll());
+        baseboardData.setWorkCounts(workCountDAO.findAll());
         return baseboardData;
     }
 }
--- src/main/java/top/dustone/kaixin/util/BaseboardData.java
+++ src/main/java/top/dustone/kaixin/util/BaseboardData.java
@@ -2,12 +2,14 @@ package top.dustone.kaixin.util;
 
 import top.dustone.kaixin.entity.view.DriverStatusCount;
 import top.dustone.kaixin.entity.view.MachineStatusCount;
+import top.dustone.kaixin.entity.view.WorkCount;
 
 import java.util.List;
 
 public class BaseboardData {
     private List<MachineStatusCount> machineStatusCounts;
     private List<DriverStatusCount> driverStatusCounts;
+    private List<WorkCount> workCounts;
 
     public List<MachineStatusCount> getMachineStatusCounts() {
         return machineStatusCounts;
@@ -25,11 +27,20 @@ public class BaseboardData {
         this.driverStatusCounts = driverStatusCounts;
     }
 
+    public List<WorkCount> getWorkCounts() {
+        return workCounts;
+    }
+
+    public void setWorkCounts(List<WorkCount> workCounts) {
+        this.workCounts = workCounts;
+    }
+
     @Override
     public String toString() {
         return "BaseboardData{" +
                 "machineStatusCounts=" + machineStatusCounts +
                 ", driverStatusCounts=" + driverStatusCounts +
+                ", workCounts=" + workCounts +
                 '}';
     }
 }
